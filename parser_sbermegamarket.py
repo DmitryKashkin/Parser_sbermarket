8e22d722116e46ce857e47852c0768a22e50dd23
DmitryKashkin <7842066@gmail.com>
2023-01-18 20:59:31 +0300

    v0.1

--- parser_sbermegamarket.py
+++ parser_sbermegamarket.py
@@ -1,11 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-# # from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver import ChromeOptions
-# from requests_html import HTMLSession
-# from pyppeteer import launch
-
 URL = 'https://sbermegamarket.ru/catalog'
 PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
