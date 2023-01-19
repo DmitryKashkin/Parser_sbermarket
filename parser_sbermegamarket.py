b6380c8df1f24d6d5c63c5685d6a23e95de7e304
DmitryKashkin <7842066@gmail.com>
2023-01-19 20:42:07 +0300

    v0.2

--- parser_sbermegamarket.py
+++ parser_sbermegamarket.py
@@ -3,20 +3,25 @@ from selenium import webdriver
 
 URL = 'https://sbermegamarket.ru/catalog'
 PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+PREFIX = 'https://sbermegamarket.ru'
 
 
-def get_content(url):
-    with webdriver.Chrome() as driver:
-        driver.get(URL)
-        soup = BeautifulSoup(driver.page_source, "lxml")
-        for item in soup.find_all('div', class_='catalog-category-cell catalog-department__category-item'):
-            # print(item)
-            url2 = item.find('a').get('href')
-            print(url2)
+def get_content(url, driver):
+    print(url)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    for item in soup.find_all('div', class_='catalog-category-cell catalog-department__category-item'):
+        get_content(PREFIX + item.find('a').get('href'), driver)
+    # while True:
+    #     if soup.find('div', class_='catalog-listing__items catalog-listing__items_divider-wide'):
+    #         # for ...
+    #     else:
+    #         break
 
 
 def main():
-    get_content(URL)
+    with webdriver.Chrome() as driver:
+        get_content(URL, driver)
 
 
 if __name__ == '__main__':
